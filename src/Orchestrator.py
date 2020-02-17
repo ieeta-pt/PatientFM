98a5bddda4b3c7d01c4d45cf7e3ceaf3dc405fb6
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-02-17 15:19:54 +0000

    Change the system structure to read XML Annotations

--- src/Orchestrator.py
+++ src/Orchestrator.py
@@ -1,7 +1,15 @@
 class Orchestrator():
-	def processTask1(files, dictionary, show=False):
+	def processTask1(files, XMLAnnotations, dictionaries, show=False):
+		"""
+		Method to handle with task 1 .
+		:param files: dictionary containing the clinical reports (key: filename)
+		:....
+
+		returns tuple(dictionary containing family members (key: filename, value: list),
+					  dictionary containing observations (key: filename, value: list))
+		"""
 		# to do
-		return 	list(), list()
+		return 	dict(), dict()
 
 	def processTask2(show=False):
 		# to do
