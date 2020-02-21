52ff3a30e3c681d12e7940d124a0b9093dbc242d
João F Silva <joaofsilva@aluno-7138.wireless.ua.pt>
2020-02-21 10:32:56 +0000

    Modified class attribute. Created individual methods to set default classes, to set observation classes, and to set family member classes

--- src/Entity.py
+++ src/Entity.py
@@ -11,59 +11,97 @@ ENTITY_CLASSES = {"O": 0,
                   "B-Observation": 1,  "I-Observation": 2,
                   "P-FamilyMember": 3, "M-FamilyMember": 4, "NA-FamilyMember": 5}
 
-def createClasses(datasetTXT, datasetXML):
+FAMILY_PLURALS = {'sisters', 'brothers', 'parents', 'mothers', 'fathers',
+                   'cousins', 'grandparents', 'grandmothers', 'grandfathers',
+                  'sons', 'childs', 'daughters', 'siblings', 'aunts', 'uncles'}
+
+def createDefaultClasses(datasetTXT):
     """
     :param datasetTXT: dict with text from txt files indexed by filename
-    :param datasetXML: dict with annotations from xml files indexed by filename
     :return: Dict with key:filename, value:list of lists with classes per sentence in the document
     """
     classesDict = {}
     for fileName in datasetTXT:
         classesDict[fileName] = []
         sentences = nltkSentenceSplit(datasetTXT[fileName], verbose=False)
-        observations = filterObservations(datasetXML[fileName])
-        print(fileName)
-        # Some documents can have no observations
-        if not observations:
-            for sentence in sentences:
-                sentence = nltkTokenize(sentence)
-                classesDict[fileName].append([int(0) for _ in sentence])
+        for sentence in sentences:
+            sentence = nltkTokenize(sentence)
+            classesDict[fileName].append([int(0) for _ in sentence])
+    return classesDict
 
-        else:
-            observationList = [nltkTokenize(observation[0]) for _, observation in observations if observations]
+def setObservationClasses(classesDict, datasetTXT, datasetXML):
+    """
+    This method updates default classes with the classes for Observation entities
+
+    :param classesDict: dict with key:filename, value:list of lists with default classes per sentence in the document
+    :param datasetTXT: dict with text from txt files indexed by filename
+    :param datasetXML: dict with annotations from xml files indexed by filename
+    :return: Dict with key:filename, value:list of lists with classes per sentence in the document
+    """
+    for fileName in datasetTXT:
+        sentences = nltkSentenceSplit(datasetTXT[fileName], verbose=False)
+        observations = filterObservations(datasetXML[fileName])
+        # Some documents can have no observations. In that case, the default class list remains unchanged
+        if observations:
+            observationList = [nltkTokenize(observation[0]) for _, observation in observations] #The [0] is to store the string instead of a list with a string
             observationIdx = 0
-            for sentence in sentences:
-                classList = []
+            for sentencePosition, sentence in enumerate(sentences):
                 tokenIdx = 0
                 sentence = nltkTokenize(sentence)
-                for token in sentence:
+                for tokenPosition, token in enumerate(sentence):
                     if observationIdx <= len(observationList) - 1:
                         if token == observationList[observationIdx][tokenIdx]:
                             if tokenIdx == 0:
-                                classList.append(ENTITY_CLASSES["B-Observation"])
+                                classesDict[fileName][sentencePosition][tokenPosition] = ENTITY_CLASSES["B-Observation"]
                                 if len(observationList[observationIdx]) == 1:
                                     observationIdx += 1
                                 else:
                                     tokenIdx += 1
                             else:
-                                classList.append(ENTITY_CLASSES["I-Observation"])
+                                classesDict[fileName][sentencePosition][tokenPosition] = ENTITY_CLASSES["I-Observation"]
                                 tokenIdx += 1
                                 if tokenIdx == len(observationList[observationIdx]):
                                     tokenIdx = 0
                                     observationIdx += 1
-                        else:
-                            classList.append(ENTITY_CLASSES["O"])
-                    else:
-                        classList.append(ENTITY_CLASSES["O"])
-                #assert values are ints
-                classList = [int(i) for i in classList]
-
-                print(classList)
-                classesDict[fileName].append(classList)
     return classesDict
 
+def setFamilyMemberClasses(classesDict, datasetTXT, datasetXML):
+    """
+    This method updates default classes with the classes for FamilyMember entities
+
+    :param classesDict: dict with key:filename, value:list of lists with default classes per sentence in the document
+    :param datasetTXT: dict with text from txt files indexed by filename
+    :param datasetXML: dict with annotations from xml files indexed by filename
+    :return: Dict with key:filename, value:list of lists with classes per sentence in the document
+    """
+    for fileName in datasetTXT:
+        sentences = nltkSentenceSplit(datasetTXT[fileName], verbose=False)
+        familyMembers = filterFamilyMembers(datasetXML[fileName])
+        # Some documents can have no family member mentions. In that case, the class list remains unchanged
+        if familyMembers:
+            familyMemberList = [(familyMember.lower(), familySide) for _, familyMember, familySide in familyMembers]
+            familyMemberIdx = 0
+            for sentencePosition, sentence in enumerate(sentences):
+                sentence = nltkTokenize(sentence)
+                for tokenPosition, token in enumerate(sentence):
+                    if familyMemberIdx <= len(familyMemberList) - 1:
+                        # Some tokens can be in the plural but the annotation is singular, we check that with the condition after "or"
+                        if token == familyMemberList[familyMemberIdx][0] or (token in FAMILY_PLURALS and token[:-1] == familyMemberList[familyMemberIdx][0]):
+                            if familyMemberList[familyMemberIdx][1] == "Paternal":
+                                classesDict[fileName][sentencePosition][tokenPosition] = ENTITY_CLASSES["P-FamilyMember"]
+                            elif familyMemberList[familyMemberIdx][1] == "Maternal":
+                                classesDict[fileName][sentencePosition][tokenPosition] = ENTITY_CLASSES["M-FamilyMember"]
+                            elif familyMemberList[familyMemberIdx][1] == "NA":
+                                classesDict[fileName][sentencePosition][tokenPosition] = ENTITY_CLASSES["NA-FamilyMember"]
+                            familyMemberIdx += 1
+    return classesDict
 
 def filterObservations(annotationDict):
     filteredList = [(annotation['spans'], annotation['mentions']) for _, annotation in annotationDict.items() if annotation['type'] == 'Observation']
+    filteredList = sorted(filteredList)
+    return filteredList
+
+def filterFamilyMembers(annotationDict):
+    filteredList = [(annotation['spans'], annotation['familyRelation'], annotation['familySide']) for _, annotation in annotationDict.items() if annotation['type'] == 'FamilyMember']
     filteredList = sorted(filteredList)
     return filteredList
