9b6ebf7b8dc8b0e85f093afbd83ce073184dfc96
João F Silva <joaofsilva@aluno-7138.wireless.ua.pt>
2020-02-21 14:42:04 +0000

    Embeddings can be created simply by running Pipeline.runPipeline()

--- src/embeddings/Pipeline.py
+++ src/embeddings/Pipeline.py
@@ -3,12 +3,22 @@
 
 import os
 from Reader import Reader
+from embeddings.Embeddings import Embeddings, writeEmbeddingsPickle, readEmbeddingsPickle
 from embeddings.Vocabulary import createVocabularyFile, createFasttextModel
-from Preprocessing import nltkSentenceSplit
+from Preprocessing import nltkSentenceSplit, nltkTokenize
+
 
 
 def runPipeline(settings):
 
+    createVocabulary(settings)
+    # createEmbeddingModels(settings)
+    for corpus in ("train", "test"):
+        createEmbeddingsPickle(settings, corpus)
+
+
+
+def createVocabulary(settings):
     if not os.path.exists(settings["embeddings"]["vocabulary_path"]):
         sentenceList = list()
         for corpus in ("train", "test"):
@@ -18,13 +28,35 @@ def runPipeline(settings):
             for fileName in filesRead:
                 sentences = nltkSentenceSplit(filesRead[fileName], verbose=False)
                 sentenceList.extend(sentence for sentence in sentences)
-
         createVocabularyFile(sentenceList, settings["embeddings"]["vocabulary_path"], verbose=False)
 
+
+def createEmbeddingModels(settings):
+    # Create smaller biowordvec embedding models
     if not (os.path.exists(settings["embeddings"]["biowordvec_original"]) or os.path.exists(settings["embeddings"]["biowordvec_normalized"])):
         createFasttextModel(settings["embeddings"]["vocabulary_path"], settings["embeddings"]["wordvec_path"],
                             settings["embeddings"]["biowordvec_original"], settings["embeddings"]["biowordvec_normalized"])
 
+
+def createEmbeddingsPickle(settings, corpus):
+    if corpus == "train": picklePath = settings["embeddings"]["train_embeddings_pickle"]
+    elif corpus == "test": picklePath = settings["embeddings"]["test_embeddings_pickle"]
+
+    if not os.path.exists(picklePath):
+        tokenizedSentenceList = list()
+        reader = Reader(dataSettings=settings, corpus=corpus)
+        filesRead = reader.loadDataSet()
+        for fileName in filesRead:
+            sentences = nltkSentenceSplit(filesRead[fileName], verbose=False)
+            for sentence in sentences:
+                sentence = nltkTokenize(sentence)
+                tokenizedSentenceList.extend([sentence])
+
+        embeddings = Embeddings(settings["embeddings"]["biowordvec_original"], settings["embeddings"]["biowordvec_normalized"],
+                                settings["embeddings"]["wordvec_size"])
+        embeddingsVec = Embeddings.wordvec_concat(tokenizedSentenceList)
+        writeEmbeddingsPickle(embeddingsVec, picklePath)
+        print("Created pickle file {}".format(picklePath))
 
 
 
