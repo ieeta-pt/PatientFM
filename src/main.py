1a1409deb07ae246dec960e4855e2a25ba87ccf1
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-03-10 15:18:07 +0000

    First version of rulebased approach only for family members detection with dirty code

--- src/main.py
+++ src/main.py
@@ -16,8 +16,10 @@ def help(show=False):
 							help='When active it show some parts of the processing during the executions (default: False)')
 	configs.add_argument('-c', '--cleaning', default=False, action='store_true', \
 							help='When active it cleans the clinical reports (default: False)')
-	configs.add_argument('-m', '--method', dest='method', type=str, \
+	configs.add_argument('-m', '--method', dest='method', type=str, default='',\
                         help='The method used to process.')	
+	configs.add_argument('-ds', '--dataset', dest='dataset', type=str, default='train',\
+                        help='The dataset used to process.')	
 	
 	executionMode = parser.add_argument_group('Execution Mode', 'Flags to select the execution mode!')
 	executionMode.add_argument('-t1', '--first', default=False, action='store_true', \
@@ -35,28 +37,49 @@ def readSettings(settingsFile):
 		raise Exception("The settings file was not found!")
 	return configuration
 
+def getMethod(settings, argsMethod):
+	if len(argsMethod) >0:
+		return [argsMethod]
+	methods = []
+	for method in settings["methods"]:
+		if settings["methods"][method] == "True":
+			methods += [method]
+	return methods
+
 def main():
 	args = help()
 	settings = readSettings(args.settings)
 	if  not args.first and \
 		not args.second:
+		print("Nothing to do, please define the method to process")
+		help(show=True)
+		exit()
+
+	methods = getMethod(settings, args.method)
+	if  len(methods) == 0:
 		print("Nothing to do, please select the execution mode")
 		help(show=True)
 		exit()
 
 	if args.first:
 		print("Execute the first subtask")
-		reader = Reader(dataSettings=settings, corpus="train")
+		reader = Reader(dataSettings=settings, corpus=args.dataset)
 		filesRead = reader.loadDataSet(cleaning=args.cleaning)
-		fmDocs, obsDocs = Orchestrator.processTask1(files 			= filesRead,
-													XMLAnnotations 	= reader.loadXMLAnnotations(filesRead),
-													dictionaries	= reader.loadDictionary(),
-													dataSettings    = settings,
-													method			= args.method,
-								    				show 			= args.showprints)
+		fmDocs = {}
+		obsDocs = {}
+		for method in methods:
+			fm, obs = Orchestrator.processTask1(files 			= filesRead,
+												XMLAnnotations 	= reader.loadXMLAnnotations(filesRead),
+												dictionaries	= reader.loadDictionary(),
+												dataSettings    = settings,
+												method			= method,
+								    			show 			= args.showprints)
+			fmDocs[method] = fm
+			obsDocs[method] = obs
+		fmDocsRes, obsDocsRes = Orchestrator.mergeResultsTask1(fmDocs, obsDocs)
 		Writer.writeTask1(resultFile 	= settings["results"]["task1"], 
-					 	  fmDocs 		= fmDocs, 
-					 	  obsDocs	 	= obsDocs)
+					 	  fmDocs 		= fmDocsRes, 
+					 	  obsDocs	 	= obsDocsRes)
 
 	if args.second:
 		print("Execute the second subtask")
