2f8b82066b7c37a7f61163d7a2f9f029b5e7710c
João F Silva <joaofsilva@Joaos-MBP.lan>
2020-03-20 19:57:17 +0000

    Pickle with sentence list

---
+++ src/models/ALBERT_BiLstmCRF/model.py
@@ -0,0 +1,288 @@
+import os
+import time
+import torch
+import pickle
+import numpy as np
+from torch import nn
+from sklearn import metrics
+
+from transformers import AlbertModel
+
+from models.utils import classListToTensor, updateProgress, valueToKey
+from models.Embedding_BiLstmCRF.utils import generateBatch, concatenateNejiClassesToEmbeddings
+from models.BiLstmCRF.decoder import Decoder
+from models.BiLstmCRF.encoder import BiLSTMEncoder
+
+class Model:
+    def __init__(self, configs, labels_dict, max_length, vocab_size, weights_matrix, device):
+        self.device = device
+        self.ENTITY_PREDICTION = configs.ENTITY_PREDICTION
+        self.max_length = max_length
+        self.entity_classes = labels_dict
+        self.output_size = len(self.entity_classes)
+        self.hidden_size = configs.hidden_size
+        self.decoder_insize = self.hidden_size * 2
+        self.batch_size = configs.batch_size
+        self.num_layers = configs.num_layers
+        self.epochs = configs.epochs
+        self.iterations_per_epoch = configs.iterations_per_epoch
+        self.learning_rate = configs.learning_rate
+
+        AlBertModelToSize = {"albert-base-v2": 768, "albert-large-v2": 1024, "albert-xlarge-v2": 2048, "albert-xxlarge-v2": 4096}
+        self.albert_model = configs.ALBERT_MODEL
+        self.albertLayer = AlbertModel.from_pretrained(self.albert_model).to(device=self.device)
+
+        self.USE_NEJI = configs.USE_NEJI
+        if self.USE_NEJI == "True":
+            self.encoder_insize = AlBertModelToSize[self.albert_model] + 1
+        else:
+            self.encoder_insize = AlBertModelToSize[self.albert_model]
+
+        self.encoder = BiLSTMEncoder(self.encoder_insize, self.hidden_size, self.output_size, batch_size=self.batch_size, num_layers=self.num_layers).to(device=self.device)
+        self.decoder = Decoder(self.decoder_insize, self.hidden_size, self.output_size, batch_size=self.batch_size, max_len=self.max_length, num_layers=self.num_layers).to(device=self.device)
+        self.criterion = nn.NLLLoss()
+        self.encoder_optimizer = torch.optim.Adam(self.encoder.parameters(), lr=self.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+        self.decoder_optimizer = torch.optim.Adam(self.decoder.parameters(), lr=self.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+
+
+        # total = sum(p.numel() for p in self.encoder.parameters() if p.requires_grad)
+        # total += sum(p.numel() for p in self.decoder.parameters() if p.requires_grad)
+        # print("Total number of trainable parameters: {}".format(total))
+
+    def train(self, train_encoded_sentences_tensors, train_labels, neji_classes=None):
+
+        r""" List with entity labels ("O":0 maps to nonentity) """
+        entity_labels = [value for key, value in self.entity_classes.items() if not key.startswith('O')]
+
+        n_iterations = self.epochs * self.iterations_per_epoch
+        current_loss = 0
+        all_losses = []
+        f1_score_accumulated = 0
+
+        start = time.time()
+        global_start = start
+        for iteration in range(n_iterations):
+
+            if self.USE_NEJI == "True":
+                encoded_sentences_tensors, sorted_batch_classes, sorted_len_units, mask, batch_neji_classes = \
+                    generateBatch(train_encoded_sentences_tensors, train_labels, self.batch_size, self.device, neji_classes)
+            else:
+                encoded_sentences_tensors, sorted_batch_classes, sorted_len_units, mask = \
+                    generateBatch(train_encoded_sentences_tensors, train_labels, self.batch_size, self.device)
+
+            sentence_embeddings = self.embeddingLayer(encoded_sentences_tensors)
+
+            # input_ids = torch.tensor(tokenizer.encode(sentence, add_special_tokens=True)).unsqueeze(0)
+            # outputs = model(input_ids=None, attention_mask=)
+
+
+
+
+            if self.USE_NEJI == "True":
+                sentence_neji_embeddings = concatenateNejiClassesToEmbeddings(sentence_embeddings, batch_neji_classes, self.device)
+                packed_input = torch.nn.utils.rnn.pack_padded_sequence(sentence_neji_embeddings, sorted_len_units, batch_first=True)
+            else:
+                packed_input = torch.nn.utils.rnn.pack_padded_sequence(sentence_embeddings, sorted_len_units, batch_first=True)
+
+            initial_state_h0c0 = self.encoder.initH0C0(self.device)
+            encoder_output, hidden_state_n, cell_state_n = self.encoder(packed_input.to(device=self.device), initial_state_h0c0)
+            crf_out, entity_out, crf_loss = self.decoder(encoder_output, sorted_batch_classes, mask, self.device)
+
+            if self.ENTITY_PREDICTION == "True":
+                entity_pred, entity_true = self.compute_entity_prediction(entity_out, sorted_len_units, sorted_batch_classes)
+                current_loss += self.perform_optimization_step_entity_pred(crf_loss, entity_pred, entity_true)
+            elif self.ENTITY_PREDICTION == "False":
+                current_loss += self.perform_optimization_step_no_entity_pred(crf_loss)
+            label_pred, label_true = self.compute_label_prediction(crf_out, sorted_len_units, sorted_batch_classes)
+
+            f1_score_accumulated += metrics.f1_score(label_pred.tolist(), label_true.tolist(), average='micro', labels=entity_labels)
+            if (iteration + 1) % self.iterations_per_epoch == 0:
+                all_losses.append(current_loss / self.iterations_per_epoch)
+                print("Epoch %d of %d | F1: %.3f%% | Average Loss: %.5f" %
+                      ((iteration + 1) / self.iterations_per_epoch, self.epochs, f1_score_accumulated / self.iterations_per_epoch * 100, current_loss / self.iterations_per_epoch))
+                current_loss = 0
+                f1_score_accumulated = 0
+                elapsed = (time.time() - start)
+                print("Time used:", elapsed)
+                start = time.time()
+
+        elapsed = (time.time() - global_start)
+        print("Completed training. Total time used: {}".format(elapsed))
+        # self.embeddingLayer = self.embeddingLayer.eval()
+        self.encoder = self.encoder.eval()
+        self.decoder = self.decoder.eval()
+
+
+    def test(self, test_encoded_sentences_tensors, test_labels, verbose=False, neji_classes=None, SINGLE_INSTANCE=False):
+        test_label_true = []
+        test_label_pred = []
+
+        for sentence_idx, encoded_sentence_tensor in enumerate(test_encoded_sentences_tensors):
+            sentence_length = encoded_sentence_tensor.shape[0]
+            x_input = torch.zeros((1, sentence_length), dtype=torch.long).to(self.device)
+            mask = torch.zeros((1, sentence_length), dtype=torch.long).to(self.device)
+            mask[0] = 1
+
+            r""" Convert numpy array with embeddings to tensor, and reshape to (sentence length x Embedding size)"""
+            if SINGLE_INSTANCE is True:
+                y_true_tensor = [test_labels.to(self.device)]
+            else:
+                y_true_tensor = [test_labels[sentence_idx].to(self.device)]
+
+            x_input[0] = encoded_sentence_tensor
+            sentence_embeddings = self.embeddingLayer(x_input)
+
+            if neji_classes is not None:
+                if SINGLE_INSTANCE is True:
+                    neji_tensor = neji_classes.to(self.device)
+                else:
+                    neji_tensor = neji_classes[sentence_idx].to(self.device)
+
+                sentence_embeddings = concatenateNejiClassesToEmbeddings(sentence_embeddings, neji_tensor, self.device)
+
+            packed_input = nn.utils.rnn.pack_padded_sequence(sentence_embeddings, torch.tensor([sentence_embeddings.shape[1]]), batch_first=True)
+
+            initial_state_h0c0 = (torch.zeros((2*self.num_layers, 1, self.hidden_size), dtype=torch.float32, device=self.device),
+                                  torch.zeros((2*self.num_layers, 1, self.hidden_size), dtype=torch.float32, device=self.device))
+
+            encoder_output, hidden_state_n, cell_state_n = self.encoder(packed_input.to(device=self.device), initial_state_h0c0)
+            crf_out, entity_out, crf_loss = self.decoder(encoder_output, y_true_tensor, mask, self.device)
+            test_label_pred.extend(crf_out[0])
+            test_label_true.extend(y_true_tensor[0][0].tolist())
+
+            if ((sentence_idx + 1) % 100 == 0) and verbose:
+                updateProgress(sentence_idx / len(test_encoded_sentences_tensors))
+        if verbose:
+            updateProgress(1)
+
+        return test_label_pred, test_label_true
+
+
+    def compute_entity_prediction(self, entity_out, sorted_len_units, sorted_batch_classes):
+        label_num = 0
+        entity_pred = torch.zeros((sorted_len_units.sum(), 2), dtype=torch.float, device=self.device)
+        entity_true = torch.zeros((sorted_len_units.sum()), dtype=torch.long, device=self.device)
+
+        for batch_sample_idx in range(self.batch_size):
+            sentence_len = sorted_len_units[batch_sample_idx]
+            entity_out_sample = entity_out[batch_sample_idx]
+            entity_sample_pred = entity_out_sample[:sentence_len, :]
+            for position in range(sentence_len.item()):
+                if sorted_batch_classes[batch_sample_idx][0][position] != self.entity_classes['O']:
+                    entity_true[label_num] = 1
+                else:
+                    entity_true[label_num] = 0
+                entity_pred[label_num] = entity_sample_pred[position, :]
+                label_num += 1
+
+        return entity_pred, entity_true
+
+
+    def compute_label_prediction(self, crf_out, sorted_len_units, sorted_batch_classes):
+        label_num = 0
+        label_pred = torch.zeros((sorted_len_units.sum()),dtype=torch.long,device=self.device)
+        label_true = torch.zeros((sorted_len_units.sum()),dtype=torch.long,device=self.device)
+
+        for batch_sample_idx in range(self.batch_size):
+            sentence_len = sorted_len_units[batch_sample_idx]
+            crf_out_sample = crf_out[batch_sample_idx]
+            label_sample_pred = crf_out_sample[:sentence_len]
+            for position in range(sentence_len.item()):
+                label_true[label_num] = sorted_batch_classes[batch_sample_idx][0][position]
+                label_pred[label_num] = label_sample_pred[position]
+                label_num += 1
+
+        return label_pred, label_true
+
+    def perform_optimization_step_entity_pred(self, crf_loss, entity_pred, entity_true):
+        entity_loss = self.criterion(entity_pred,entity_true)
+        loss = crf_loss + 0.2 * entity_loss
+        if self.embeddingLayer.weight.requires_grad is True:
+            self.embedding_optimizer.zero_grad()
+        self.encoder_optimizer.zero_grad()
+        self.decoder_optimizer.zero_grad()
+        loss.backward()
+        if self.embeddingLayer.weight.requires_grad is True:
+            self.embedding_optimizer.step()
+        self.encoder_optimizer.step()
+        self.decoder_optimizer.step()
+        return loss.item()
+
+    def perform_optimization_step_no_entity_pred(self, crf_loss):
+        loss = crf_loss
+        if self.embeddingLayer.weight.requires_grad is True:
+            self.embedding_optimizer.zero_grad()
+        self.encoder_optimizer.zero_grad()
+        self.decoder_optimizer.zero_grad()
+        loss.backward()
+        if self.embeddingLayer.weight.requires_grad is True:
+            self.embedding_optimizer.step()
+        self.encoder_optimizer.step()
+        self.decoder_optimizer.step()
+        return loss.item()
+
+
+    def get_entity_type_count(self, testLabels):
+        r""" This process works by trimming the first part of the label string (B-/I-/P-/M-/NA-),
+        resulting in the label with the entity type
+        POSITIVE_CLASSES: B-Observation, I-Observation, P-FamilyMember, M-FamilyMember, NA-FamilyMember"""
+
+        entityDict = {}
+        for labelIdx, testLabel in enumerate(testLabels):
+            label = valueToKey(testLabel, self.entity_classes)
+            labelPrefix = label[:2]
+            if labelPrefix in ("P-", "M-", "NA"):
+                entityType = "FamilyMember"
+                entityDict[(labelIdx, labelIdx)] = entityType
+            elif labelPrefix == "B-":
+                entityType = "Observation"
+                nextLabelIdx = labelIdx + 1
+                nextLabel = valueToKey(testLabels[nextLabelIdx], self.entity_classes)
+                if nextLabel != "I-Observation":
+                    entityDict[(labelIdx, labelIdx)] = entityType
+                elif nextLabel == "I-Observation":
+                    while nextLabel == "I-Observation":
+                        nextLabelIdx += 1
+                        if nextLabelIdx <= (len(testLabels)-1):
+                            nextLabel = valueToKey(testLabels[nextLabelIdx], self.entity_classes)
+                        else:
+                            nextLabel = "EndSentence"
+                    entityDict[(labelIdx, nextLabelIdx-1)] = entityType
+
+        entityTypeCount = {'Observation': 0, 'FamilyMember': 0}
+        for _, entityType in entityDict.items():
+            entityTypeCount[entityType] += 1
+
+        return entityDict, entityTypeCount
+
+
+    def evaluate_test(self, testLabelPred, testLabelTrue):
+        labels = [value for key, value in self.entity_classes.items() if key != "O"] # NO MODELO ORIGINAL USAVAM COM ESTA CONDIÇÃO, SEM A CLASSE 0
+        print(metrics.classification_report(testLabelTrue, testLabelPred, labels=labels))
+
+        trueEntity, trueEntityTypeCount = self.get_entity_type_count(testLabelTrue)
+        predEntity, predEntityTypeCount = self.get_entity_type_count(testLabelPred)
+
+        exactHits = {'Observation': 0, 'FamilyMember': 0}
+        for key, entity_type in trueEntity.items():
+            if key in predEntity.keys() and predEntity[key] == entity_type:
+                exactHits[entity_type] += 1
+
+        entityTypes = ['Observation', 'FamilyMember']
+        recall = [exactHits[entityType] / trueEntityTypeCount[entityType] for entityType in entityTypes]
+        precision = [exactHits[entityType] / predEntityTypeCount[entityType] for entityType in entityTypes]
+        fp = np.mean([(2 * precision[i] * recall[i]) / (precision[i] + recall[i]) for i in range(len(entityTypes))])
+        print("Exact Hit metrics: F1 = {} | Precision = {} | Recall = {}".format(round(fp, 4), round(np.mean(precision), 4), round(np.mean(recall), 4)))
+
+
+    def write_model_files(self, test_label_pred, test_label_true, seed):
+        timepoint = time.strftime("%Y%m%d_%H%M")
+        path = '../results/models/Embedding_BiLstmCRF/'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename = 'N2C2_BioWordVec_' + timepoint + '-' + str(seed)
+        pickle.dump([test_label_pred, test_label_true], open(path + "results-" + filename + '.pickle', 'wb'))
+        torch.save(self.embeddingLayer, path + 'model_embedding_layer-'+filename)
+        torch.save(self.encoder, path + 'model_encoder-'+filename)
+        torch.save(self.decoder, path + 'model_decoder-'+filename)
+
---
+++ src/models/ALBERT_BiLstmCRF/modelRunners.py
@@ -0,0 +1,199 @@
+import torch
+import random
+import numpy as np
+from sklearn.model_selection import KFold
+
+from Reader import Reader
+from NejiAnnotator import readPickle
+from Entity import createTrueClasses, createDefaultClasses, ENTITY_CLASSES
+
+from models.utils import classListToTensor, classDictToList, getSentenceList, mergeDictionaries, createOutputTask1
+
+from models.AlBert_BiLstmCRF.utils import loadModelConfigs, getSentenceListWithMapping
+from models.AlBert_BiLstmCRF.model import Model
+
+from transformers import AlbertTokenizer
+
+def runModel(settings, trainTXT, trainXML):
+    """ Trains the model in the FULL training dataset and computes predictions for the FULL test set
+    :param settings: settings from settings.ini file
+    :param trainTXT: train txts
+    :param trainXML: train xml annotations
+    :return: finalFamilyMemberDict, finalObservationsDict: dicts indexed by filename with detected entities
+    """
+
+    seed = [35899,54377,66449,77417,29,229,1229,88003,99901,11003]
+    random_seed = seed[9]
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+
+    torch.cuda.is_available()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+
+
+    print("Loading and preprocessing data.\n")
+
+    embeddingModel = np.load(settings["embeddings"]["biowordvec_original"], allow_pickle=True).item()
+    vocab = [key for key in embeddingModel]
+    vocabSize = len(vocab)
+    embeddingsWeightsMatrix = np.zeros((vocabSize, int(settings["embeddings"]["wordvec_size"])))
+    word2Idx = {}
+    for i, word in enumerate(vocab):
+        word2Idx[word] = i
+        embeddingsWeightsMatrix[i] = embeddingModel[word]
+    embeddingsWeightsMatrix = torch.tensor(embeddingsWeightsMatrix, dtype=torch.float64).to(device)
+
+    trainTokenizedSentences = getSentenceList(trainTXT, tokenized=True)
+    trainEncodedSentences = []
+    for sentence in trainTokenizedSentences:
+        sentence = [word2Idx[token] for token in sentence]
+        trainEncodedSentences.append(torch.tensor(sentence, dtype=torch.long).to(device))
+
+    trainClassesDict = createTrueClasses(trainTXT, trainXML)
+    trainClasses = classDictToList(trainClassesDict)
+    trainClasses = [classListToTensor(sentenceClasses, datatype=torch.long) for sentenceClasses in trainClasses]
+
+    if settings["neji"]["use_neji_annotations"] == "True":
+        nejiTrainClassesDict = readPickle(settings["neji"]["neji_train_pickle"])
+        nejiTrainClasses = classDictToList(nejiTrainClassesDict)
+        nejiTrainClasses = [classListToTensor(sentenceClasses, datatype=torch.float) for sentenceClasses in nejiTrainClasses]
+
+# 100 is the default size used in embedding creation
+    max_length = 100
+    print("Loaded data successfully.\n")
+
+    modelConfigs = loadModelConfigs(settings)
+
+    DL_model = Model(modelConfigs, ENTITY_CLASSES, max_length, vocabSize, embeddingsWeightsMatrix, device)
+    print("Model created. Starting training.\n")
+    DL_model.train(trainEncodedSentences, trainClasses, neji_classes=nejiTrainClasses)
+
+    print("Starting the testing phase.\n")
+    reader = Reader(dataSettings=settings, corpus="test")
+    testTXT = reader.loadDataSet()
+
+    testClassesDict = createDefaultClasses(testTXT)
+    testClasses = classDictToList(testClassesDict)
+    testClasses = [classListToTensor(sentenceClasses, datatype=torch.long) for sentenceClasses in testClasses]
+
+    if settings["neji"]["use_neji_annotations"] == "True":
+        nejiTestClassesDict = readPickle(settings["neji"]["neji_test_pickle"])
+        nejiTestClasses = classDictToList(nejiTestClassesDict)
+        nejiTestClasses = [classListToTensor(sentenceClasses, datatype=torch.float) for sentenceClasses in nejiTestClasses]
+
+    testTokenizedSentences, testSentenceToDocList = getSentenceListWithMapping(testTXT, tokenized=True)
+    testEncodedSentences = []
+    for sentence in testTokenizedSentences:
+        sentence = [word2Idx[token] for token in sentence]
+        testEncodedSentences.append(torch.tensor(sentence, dtype=torch.long).to(device))
+
+    predFamilyMemberDict, predObservationDict = createOutputTask1(DL_model, testTokenizedSentences, testEncodedSentences,
+                                                                  testClasses, testSentenceToDocList, neji_classes=nejiTestClasses)
+    return predFamilyMemberDict, predObservationDict
+
+
+
+
+
+
+def runModelDevelopment(settings, trainTXT, trainXML, cvFolds):
+    """ Trains the model with K-fold cross validation, using K-1 splits to train and 1 to validate (and generate output), in K possible combinations.
+    :param settings: settings from settings.ini file
+    :param trainTXT: train txts
+    :param trainXML: train xml annotations
+    :param cvFolds: number of folds for cross validation
+    :return: finalFamilyMemberDict, finalObservationsDict: dicts indexed by filename with detected entities
+    """
+
+    seed = [35899,54377,66449,77417,29,229,1229,88003,99901,11003]
+    random_seed = seed[9]
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+
+    torch.cuda.is_available()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+
+    print("Loading and preprocessing data.\n")
+
+    tokenizer = AlbertTokenizer.from_pretrained(settings["ALBERT"]["model"])
+
+
+    encodedTokenizedSentences, sentenceToDocList = getSentenceListWithMapping(trainTXT, tokenizer, device)
+
+
+    classesDict = createTrueClasses(trainTXT, trainXML)
+    classes = classDictToList(classesDict)
+    classes = [classListToTensor(sentenceClasses, datatype=torch.long) for sentenceClasses in classes]
+
+    if settings["neji"]["use_neji_annotations"] == "True":
+        nejiClassesDict = readPickle(settings["neji"]["neji_train_pickle"])
+        nejiClasses = classDictToList(nejiClassesDict)
+        nejiClasses = [classListToTensor(sentenceClasses, datatype=torch.float) for sentenceClasses in nejiClasses]
+
+    kFolds = KFold(n_splits=cvFolds)
+    predFamilyMemberDicts = []
+    predObservationsDicts = []
+
+    print("Beginning KFold cross validation.\n")
+
+    for trainIdx, testIdx in kFolds.split(encodedTokenizedSentences):
+        trainEncodedSentences = [encodedTokenizedSentences[idx] for idx in trainIdx]
+
+        trainClasses = [classes[idx] for idx in trainIdx]
+
+        testTokenizedSentences = [tokenizedSentences[idx] for idx in testIdx]
+        testEncodedSentences = [encodedSentences[idx] for idx in testIdx]
+        testClasses = [classes[idx] for idx in testIdx]
+        testDocMapping = [sentenceToDocList[idx] for idx in testIdx]
+
+        if settings["neji"]["use_neji_annotations"] == "True":
+            nejiTrainClasses = [nejiClasses[idx] for idx in trainIdx]
+            nejiTestClasses = [nejiClasses[idx] for idx in testIdx]
+        else:
+            nejiTrainClasses = None
+            nejiTestClasses = None
+
+        # 100 is the default size used in embedding creation
+        max_length = 100
+        print("Loaded data successfully.\n")
+
+        modelConfigs = loadModelConfigs(settings)
+
+        DL_model = Model(modelConfigs, ENTITY_CLASSES, max_length, vocabSize, embeddingsWeightsMatrix, device)
+        print("Model created. Starting training.\n")
+        DL_model.train(trainEncodedSentences, trainClasses, neji_classes=nejiTrainClasses)
+
+        # DL_model.train_time_debug(trainTokenizedSentences, trainEmbeddings, trainClasses)
+
+        print("Starting the testing phase.\n")
+        testLabelPred, testLabelTrue = DL_model.test(testEncodedSentences, testClasses, neji_classes=nejiTestClasses)
+        print("Finished the testing phase. Evaluating test results\n")
+        DL_model.evaluate_test(testLabelPred, testLabelTrue)
+        print("Writing model files to disk.\n")
+        DL_model.write_model_files(testLabelPred, testLabelTrue, seed)
+
+        print("Generating prediction output for final tsv.\n")
+        predFamilyMemberDict, predObservationDict = createOutputTask1(DL_model, testTokenizedSentences,
+                                                                      testEncodedSentences, testClasses,
+                                                                      testDocMapping, neji_classes=nejiTestClasses)
+        predFamilyMemberDicts.append(predFamilyMemberDict)
+        predObservationsDicts.append(predObservationDict)
+
+    finalFamilyMemberDict = mergeDictionaries(predFamilyMemberDicts)
+    finalObservationsDict = mergeDictionaries(predObservationsDicts)
+
+    return finalFamilyMemberDict, finalObservationsDict
