6f8c98abbef18ec5143d9beccb9a0ed5cb1b728c
João F Silva <joaofsilva@Joaos-MBP.lan>
2020-10-16 14:18:29 +0100

    Code cleaning

--- src/models/Embedding_BiLstmCRF/utils.py
+++ src/models/Embedding_BiLstmCRF/utils.py
@@ -19,32 +19,6 @@ def loadModelConfigs(settings):
     return configs
 
 
-r""" This method does not ensure that the whole dataset is used, it just uses random numbers the whole time to sample """
-# def generateBatch(tokenized_sentences_tensors, classes, batch_size, device):
-#
-#     batch_idx = np.random.randint(low=0,high=len(tokenized_sentences_tensors), size=batch_size).tolist()
-#     batch_tokenized_sentences_tensors = [tokenized_sentences_tensors[i] for i in batch_idx]
-#     batch_classes = [torch.LongTensor(classes[i]) for i in batch_idx]
-#
-#     sentences_length = torch.LongTensor([sentence_tensor.shape[0] for sentence_tensor in batch_tokenized_sentences_tensors])
-#     sentences_tensor = torch.zeros((batch_size, sentences_length.max()), dtype=torch.long).to(device)
-#     mask = torch.zeros((batch_size, sentences_length.max()), dtype=torch.long).to(device)
-#
-#     for idx, (sentence_idx, sentence_length) in enumerate(zip(batch_idx, sentences_length)):
-#         sentences_tensor[idx, :sentence_length] = batch_tokenized_sentences_tensors[idx]
-#         mask[idx, :sentence_length] = 1
-#
-#     sorted_len_units, perm_idx = sentences_length.sort(0, descending=True)
-#     sentences_tensor = sentences_tensor[perm_idx]
-#     mask = mask[perm_idx]
-#
-#     sorted_batch_classes = []
-#     for idx in perm_idx:
-#         sorted_batch_classes.append(batch_classes[idx])
-#
-#     return sentences_tensor, sorted_batch_classes, sorted_len_units, mask
-
-
 def generateBatch(tokenized_sentences_tensors, classes, batch_size, device, neji_classes=None):
 
     batch_idx = np.random.randint(low=0,high=len(tokenized_sentences_tensors), size=batch_size).tolist()
@@ -89,6 +63,6 @@ def concatenateNejiClassesToEmbeddings(embeddings, nejiClasses, device):
     :param device:
     :return:
     """
-    nejiTensor = nejiClasses.unsqueeze(2)/8.to(device)
+    nejiTensor = (nejiClasses.unsqueeze(2)/8).to(device)
     newTensor = torch.cat((embeddings, nejiTensor), dim=2)
     return newTensor
--- src/models/utils.py
+++ src/models/utils.py
@@ -98,39 +98,6 @@ def generateBatch(tokenized_sentences, sentences_embeddings, embedding_dimension
     return sentences_tensor, sorted_batch_classes, sorted_len_units, packed_input, mask
 
 
-# def createOutputTask1(DLmodel, testTokenizedSentences, testEncodedSentences, testClasses, testDocMapping, neji_classes=None):
-#     """
-#     Runs the trained model on the unseen data split (validation or test), returning the resulting entity predictions
-#     :param DLmodel:
-#     :param testTokenizedSentences:
-#     :param testEncodedSentences:
-#     :param testClasses:
-#     :param testDocMapping:
-#     :param neji_classes: neji classes in case neji annotations are used to add input information to the model
-#     :return:
-#     """
-#
-#     predFamilyMemberDict = {}
-#     predObservationDict = {}
-#     for idx, _ in enumerate(testTokenizedSentences):
-#         if neji_classes is not None:
-#             nejiClasses = neji_classes[idx]
-#         else:
-#             nejiClasses = None
-#
-#         testModelPred, _ = DLmodel.test([testEncodedSentences[idx]], testClasses[idx], SINGLE_INSTANCE=True, neji_classes=nejiClasses)
-#         familyMemberList, observationsList = predictionToOutputTask1(testModelPred, testTokenizedSentences[idx])
-#         if familyMemberList:
-#             if testDocMapping[idx] not in predFamilyMemberDict.keys():
-#                 predFamilyMemberDict[testDocMapping[idx]] = []
-#             predFamilyMemberDict[testDocMapping[idx]].extend(familyMemberList)
-#         if observationsList:
-#             if testDocMapping[idx] not in predObservationDict.keys():
-#                 predObservationDict[testDocMapping[idx]] = []
-#             predObservationDict[testDocMapping[idx]].extend(observationsList)
-#     return predFamilyMemberDict, predObservationDict
-
-
 def createOutputTask1(DLmodel, testTokenizedSentences, testEncodedSentences, testClasses, testDocMapping, neji_classes=None):
     """
     Runs the trained model on the unseen data split (validation or test), returning the resulting entity predictions
